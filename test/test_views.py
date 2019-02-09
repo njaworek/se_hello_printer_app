5701547638914384800d9ccc6a4463c711cb3e10
njaworek <njaworek@users.noreply.github.com>
2019-02-09 14:55:30 +0100

    zmiany na brachu dodanie mozliwosci podania imienia

--- test/test_views.py
+++ test/test_views.py
@@ -18,5 +18,5 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         imie_test = 'Maria'
-        rv = self.app.get('/?output=json&imie =' + imie_test)
-        self.assertEquals('{ "imie":"' + imie_test + '" , "msg":Witaj swiecie!"}', rv.data)
+        rv = self.app.get('/?output=json&imie=' + imie_test)
+        self.assertEquals('{ "imie":"' + imie_test + '", "msg":Witaj swiecie!"}', rv.data)
